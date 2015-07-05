6e6b203bab260e4c49c2a9e3df4c9612b03a1952
lee-egist <e.gist2@gmail.com>
2015-07-05 12:54:28 -0700

    Create callable function2

--- projects/PluckyDisaster.js
+++ projects/PluckyDisaster.js
@@ -87,7 +87,11 @@ Plucky.prototype.reroll = function () {
 function play(){
 game = new Elevator();
 game.myInfo();
+getbutton();
 };
+function getButton(){
+  document.getElementById('roll').disabled = false
+}
 function roll(){
   game.roll();
 };
