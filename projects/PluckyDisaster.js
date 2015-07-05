e7743f744f245135b23068e50b50ce5ebd01fbd4
lee-egist <e.gist2@gmail.com>
2015-07-05 12:01:34 -0700

    Create callable function

--- projects/PluckyDisaster.js
+++ projects/PluckyDisaster.js
@@ -22,7 +22,7 @@ Elevator.prototype.go_down = function () {
 
 Elevator.prototype.myInfo = function () {
   // body...
-  getElementById('results').innerHTML =  console.log('You were trying to get to floor', this.player.desiredFloor);
+  document.getElementById('results').innerHTML =  console.log('You were trying to get to floor', this.player.desiredFloor);
 };
 
 Elevator.prototype.roll = function () {
