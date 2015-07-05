5d76b6e2e9b9329457fd6b85f1a68f7f2b662aa2
lee-egist <e.gist2@gmail.com>
2015-07-05 15:39:26 -0700

    Add project stylesheets

--- projects/PluckyDisaster.js
+++ projects/PluckyDisaster.js
@@ -5,7 +5,7 @@ function Elevator() {
   this.child = new Plucky;
   this.player = new Player;
   this.rollcount = 0;
-  this.yourtrys = 5;
+  this.yourtrys = 4;
 }
 
 Elevator.prototype.go_up = function () {
