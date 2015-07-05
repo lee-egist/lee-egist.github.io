71c2c8095ff23c41e6d21060c7412cde8f2402d5
lee-egist <e.gist2@gmail.com>
2015-07-05 14:36:56 -0700

    Add project stylesheets

--- projects/PluckyDisaster.js
+++ projects/PluckyDisaster.js
@@ -1,13 +1,11 @@
 function Elevator() {
   // body...
-  this.floors = Math.floor(Math.random() * 100) + 1 ;
-  this.currentFloor = Math.floor(this.floors / 2);
-  this.startingFloor = Math.floor(this.floors / 2);
+  this.currentFloor = (Math.random() * 100) + 1;
+  this.startingFloor = Math.floor(this.currentFloor / 5);
   this.child = new Plucky;
   this.player = new Player;
   this.rollcount = 0;
-  this.diff = 5;
-  this.yourtrys = this.diff * 2
+  this.yourtrys = 3;
 }
 
 Elevator.prototype.go_up = function () {
@@ -22,28 +20,42 @@ Elevator.prototype.go_down = function () {
 
 Elevator.prototype.myInfo = function () {
   // body...
+  console.log('current floor', this.currentFloor);
+  console.log('desired floor', this.player.desiredFloor);
   var goal = ['Goal: You are trying to get to floor', this.player.desiredFloor].join(" ");
   var start = ['You are on the', this.startingFloor, 'floor'].join(" ");
 
-  document.getElementById('results').innerHTML = [goal, start].join("<br>");
+  // document.getElementById('results').innerHTML = [goal, start].join("<br>");
 };
 
 Elevator.prototype.roll = function () {
   // body...
-  if (this.currentFloor != this.player.desiredFloor && this.player.desiredFloor < this.currentFloor) {
-    rollcount += 1;
-    calculateCurrentFloor();
-    this.currentFloor -= this.player.wins;
+  this.rollcount += 1;
+  this.diff = Math.abs(this.currentFloor - this.player.desiredFloor);
+  if (this.currentFloor != this.player.desiredFloor && this.player.desiredFloor > this.currentFloor) {
+    this.child.reroll();
+    this.rollcount += 1;
+    for (var i = 0; i < this.diff / 2; i++) {
+      this.calculateCurrentFloor();
+    }
+    for (var i = 0; i < this.player.wins; i++) {
+      this.currentFloor += 1;
+      if (this.currentFloor == this.player.desiredFloor){
+        console.log('the player has won');
+        var results = 'The player has won!';
+        var survived = ['You made it through', this.rollcount , 'floors'].join(" ");
+        document.getElementById('results').innerHTML = [results, survived].join("<br>");
+        break;
+      }
+    }
+    console.log(this.currentFloor);
 
-  } else if (this.currentFloor != this.player.desiredFloor && this.player.desiredFloor > this.currentFloor){
-    rollcount += 1;
-    calculateCurrentFloor();
-    this.currentFloor += this.player.wins;
   } else if(this.currentFloor == this.player.desiredFloor){
       var results = 'The player has won!';
       var survived = ['You made it through', this.rollcount , 'floors'].join(" ");
+      console.log('You won');
       document.getElementById('results').innerHTML = [results, survived].join("<br>");
-  } else if(this.rollcount >= this.diff){
+  } else if(this.rollcount >= this.yourtrys){
     var mytest = Math.abs(this.diff - this.rollcount);
     var results = ('Sorry. You died of starvation.');
     var test = ['Your current floor was:', this.currentFloor].join(" ");
@@ -51,48 +63,12 @@ Elevator.prototype.roll = function () {
     document.getElementById('results').innerHTML = [results, test, survived].join("<br>");
     document.getElementById('roll').disabled = true;
   }
-  // if(this.currentFloor != this.player.desiredFloor && this.rollcount < this.yourtrys){
-  //   this.child.reroll();
-  //
-  //   if (this.player.desiredFloor < this.currentFloor) {
-  //
-  //     if (this.child.chanceOfPushingButton > 75) {
-  //       this.go_up();
-  //       this.rollcount += 1;
-  //       this.roll();
-  //     } else {
-  //       this.go_down();
-  //       this.roll();
-  //
-  //     }
-  //   } else {
-  //     if(this.child.chanceOfPushingButton > 75) {
-  //       this.go_down();
-  //       this.rollcount += 1;
-  //       this.roll();
-  //     } else {
-  //       this.go_up();
-  //       this.roll();
-  //     }
-  //   }
-  //
-  // } else if(this.currentFloor == this.player.desiredFloor){
-  //   var results = 'The player has won!';
-  //   var survived = ['You made it through', this.rollcount , 'floors'].join(" ");
-  //   document.getElementById('results').innerHTML = [results, survived].join("<br>");
-  // } else if(this.rollcount >= this.diff){
-  //   var mytest = Math.abs(this.diff - this.rollcount);
-  //   var results = ('Sorry. You died of starvation.');
-  //   var test = ['Your current floor was:', this.currentFloor].join(" ");
-  //   var survived = ['You made it through', this.rollcount , 'floors'].join(" ");
-  //   document.getElementById('results').innerHTML = [results, test, survived].join("<br>");
-  // }
 
 };
 Elevator.prototype.calculateCurrentFloor = function () {
   // body...
   this.child.reroll();
-  if (this.child.chanceOfPushingButton > 75) {
+  if (this.child.chanceOfPushingButton > 60) {
     this.child.wins += 1;
   } else {
     this.player.wins += 1;
@@ -100,7 +76,7 @@ Elevator.prototype.calculateCurrentFloor = function () {
 };
 
 function Player(number){
-  this.desiredFloor = Math.floor((Math.random() * 100) + 1);
+  this.desiredFloor = 87;
   this.wins = 0;
 };
 
@@ -123,3 +99,11 @@ document.getElementById('roll').disabled = false;
 function roll(){
   game.roll();
 };
+
+thisgame = new Elevator();
+thisgame.myInfo();
+thisgame.roll();
+thisgame.myInfo();
+thisgame.roll();
+thisgame.myInfo();
+thisgame.roll();
