fe326d69a72f7e016983a38fbcc6d5f80acaf221
lee-egist <e.gist2@gmail.com>
2015-07-13 16:59:47 -0700

    Add timer

--- js/timer.js
+++ js/timer.js
@@ -1,8 +1,20 @@
-function timer() {
-  // body...
-  var startDate = new Date(2015, 7, 13)
-  var mytime = Date.now();
-  var timeLeft = startDate - mytime;
-  document.getElementById('timer').innerHTML = timeLeft.getMinutes();
-}
-timer();
+// var myTime = new Date();
+// console.log(myTime);
+// var startDate = new Date(2015, 6, 13);
+// console.log(startDate);
+// var timeleft = myTime - startDate;
+// var countdown = new Date(timeleft)
+// console.log(countdown.getUTCHours(), countdown.getMinutes(), countdown.getSeconds());
+
+function countdown(){
+  var myTime = new Date();
+  var end = new Date(2015, 7, 3);
+  console.log(end);
+  var count = end - myTime;
+  console.log(count);
+  var timeleft = new Date(count);
+  console.log(timeleft.getDate());
+
+  document.getElementById('timer').innerHTML = (timeleft.getDate(), ":", timeleft.getUTCHours(), ":", timeleft.getMinutes(),":", timeleft.getSeconds());
+
+};
