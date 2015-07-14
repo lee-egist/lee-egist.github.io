f9cf8a90fff96f29c924900957bbe17ec0fc417c
lee-egist <e.gist2@gmail.com>
2015-07-13 18:19:00 -0700

    Add timer2

--- js/timer.js
+++ js/timer.js
@@ -10,18 +10,25 @@ function countdown(){
   var myText = [days, hours, min, sec].join(":");
   document.getElementById('timer').innerHTML = myText;
 };
+
 function count(){
   setInterval(function() {
     var myTime = new Date();
     var end = new Date(2015, 7, 3);
     var count = end - myTime;
     var timeleft = new Date(count);
-    var days = '%02d'.format(timeleft.getDate());
-    var hours = '%02d'.format(timeleft.getUTCHours());
-    var min = '%02d'.format(timeleft.getMinutes());
-    var sec = '%02d'.format(timeleft.getSeconds());
+    var days = paddnum(timeleft.getDate());
+    var hours = paddnum(timeleft.getUTCHours());
+    var min = paddnum(timeleft.getMinutes());
+    var sec = paddnum(timeleft.getSeconds());
     var myText = [days, hours, min, sec].join(":");
     var my_sentence = ["Countdown to Onsite ", myText].join("- ")
     document.getElementById('timer').innerHTML = my_sentence;
   }, 1000);
+};
+function paddnum(num){
+  if (num.toString.length != 2){
+    num = ["0", num].join('');
+  }
+  return num;
 };
