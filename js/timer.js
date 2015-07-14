f9b41deedc9928ea4ba988d847813b631fa3f501
lee-egist <e.gist2@gmail.com>
2015-07-13 18:09:10 -0700

    Add timer2

--- js/timer.js
+++ js/timer.js
@@ -16,10 +16,10 @@ function count(){
     var end = new Date(2015, 7, 3);
     var count = end - myTime;
     var timeleft = new Date(count);
-    var days = timeleft.getDate();
-    var hours = timeleft.getUTCHours();
-    var min = timeleft.getMinutes();
-    var sec = timeleft.getSeconds();
+    var days = '%2d'.format(timeleft.getDate());
+    var hours = '%2d'.format(timeleft.getUTCHours());
+    var min = '%2d'.format(timeleft.getMinutes());
+    var sec = '%2d'.format(timeleft.getSeconds());
     var myText = [days, hours, min, sec].join(":");
     var my_sentence = ["Countdown to Onsite ", myText].join("- ")
     document.getElementById('timer').innerHTML = my_sentence;
