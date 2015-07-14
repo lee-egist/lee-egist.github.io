06148e6aaa385957fcc260c68dbe17f21fffee30
lee-egist <e.gist2@gmail.com>
2015-07-13 18:03:28 -0700

    Add timer2

--- js/timer.js
+++ js/timer.js
@@ -1,13 +1,27 @@
 function countdown(){
   var myTime = new Date();
   var end = new Date(2015, 7, 3);
-  console.log(end);
   var count = end - myTime;
-  console.log(count);
   var timeleft = new Date(count);
-  console.log(timeleft.getDate());
-
-  document.getElementById('timer').innerHTML = (timeleft.getDate(), ":", timeleft.getUTCHours(), ":", timeleft.getMinutes(),":", timeleft.getSeconds());
-  console.log(timeleft.getDate(), ":", timeleft.getUTCHours(), ":", timeleft.getMinutes(),":", timeleft.getSeconds());
-
+  var days = timeleft.getDate();
+  var hours = timeleft.getUTCHours();
+  var min = timeleft.getMinutes();
+  var sec = timeleft.getSeconds();
+  var myText = [days, hours, min, sec].join(":");
+  document.getElementById('timer').innerHTML = myText;
+};
+function count(){
+  setInterval(function() {
+    var myTime = new Date();
+    var end = new Date(2015, 7, 3);
+    var count = end - myTime;
+    var timeleft = new Date(count);
+    var days = timeleft.getDate();
+    var hours = timeleft.getUTCHours();
+    var min = timeleft.getMinutes();
+    var sec = timeleft.getSeconds();
+    var myText = [days, hours, min, sec].join(":");
+    var my_sentence = ["Countdown to Onsite ", myText].join("- ")
+    document.getElementById('timer').innerHTML = my_sentence;
+  }, 1000);
 };
