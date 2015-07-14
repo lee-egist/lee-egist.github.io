758879978f6246c081cb31730aa531c8dc1209f8
lee-egist <e.gist2@gmail.com>
2015-07-13 17:17:46 -0700

    Add timer2

--- js/timer.js
+++ js/timer.js
@@ -1,11 +1,3 @@
-// var myTime = new Date();
-// console.log(myTime);
-// var startDate = new Date(2015, 6, 13);
-// console.log(startDate);
-// var timeleft = myTime - startDate;
-// var countdown = new Date(timeleft)
-// console.log(countdown.getUTCHours(), countdown.getMinutes(), countdown.getSeconds());
-
 function countdown(){
   var myTime = new Date();
   var end = new Date(2015, 7, 3);
@@ -15,7 +7,7 @@ function countdown(){
   var timeleft = new Date(count);
   console.log(timeleft.getDate());
 
-  document.getElementById('timer').innerHTML = timeleft.getDate(), ":", timeleft.getUTCHours(), ":", timeleft.getMinutes(),":", timeleft.getSeconds();
+  document.getElementById('timer').innerHTML = (timeleft.getDate(), ":", timeleft.getUTCHours(), ":", timeleft.getMinutes(),":", timeleft.getSeconds());
   console.log(timeleft.getDate(), ":", timeleft.getUTCHours(), ":", timeleft.getMinutes(),":", timeleft.getSeconds());
 
 };
